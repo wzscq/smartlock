e0d7bfd8a7fdb58df188416dabd906b61d6642b7
wangzhongshu <wzscq@126.com>
2023-04-23 13:07:14 +0800

    commit changes

--- service/lockhub/client.go
+++ service/lockhub/client.go
@@ -26,23 +26,51 @@ func SendCommand(server,cmd string,timeoutDuration time.Duration)(string,error){
         return "",err
     }
 
+    //先接收数据，把缓存中的数据清空
+    // 接收数据
+    buffer := make([]byte, 1024)
+    for {
+        length, err := conn.Read(buffer)
+        if err != nil {
+            conn.Close()
+            connect_mutex.Unlock()
+            return "",err
+        }
+        if length == 0 {
+            break
+        } 
+        response := string(buffer[:length])
+        log.Println("receive before send commnd", response)
+    }
+
 	log.Println("send command：", cmd)
     // 发送数据
     conn.Write([]byte(cmd))
 
     // 接收数据
-    buffer := make([]byte, 1024)
-    length, err := conn.Read(buffer)
-    if err != nil {
-		conn.Close()
-		connect_mutex.Unlock()
-        return "",err
+    //buffer := make([]byte, 1024)
+    receiveLength:=0
+    for {
+        length, err := conn.Read(buffer[receiveLength:])
+        if err != nil {
+            conn.Close()
+            connect_mutex.Unlock()
+            return "",err
+        }
+        if length == 0 {
+            break
+        }
+        receiveLength+=length
     }
-    response := string(buffer[:length])
+    response := string(buffer[:receiveLength])
     log.Println("receive buffer", response)
     resList:=strings.SplitAfter(response,"END")
     log.Println("receive resList", resList,len(resList))
-    response=resList[len(resList)-2]
+    if len(resList)>=2 {
+        response=resList[len(resList)-2]
+    } else {
+        response=resList[0]
+    }
     log.Println("receive response：", response)
 	conn.Close()
 	connect_mutex.Unlock()
